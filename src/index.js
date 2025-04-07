941d6c633d967991ff382ac958da0994e408935b
brittLiban <britt.liban@student.greenriver.edu>
2025-04-06 19:36:38 -0700

    No idea why its not working...

--- src/index.js
+++ src/index.js
@@ -20,6 +20,52 @@ const reviews = [
       "The coffee was great but the employees didn't let me stay past closing! ): Worst experience ever.",
   },
 ];
+
+
+
 /////////////////////////////////////////////////////////////////////
 
 //Your Code Below Here////
+
+import calculateStarAverage from './logic.js'
+
+const reviewsSection = document.querySelector(".reviews");
+
+let renderReviews = (review) => {
+
+  let review_container = document.createElement('div');
+  review_container.classList.add("review_container");
+
+
+  let img = document.createElement('img');
+  img.src = `${review.image}`
+  review_container.append(img);
+
+  let loopDiv = document.createElement('div');
+  review_container.append(loopDiv);
+
+
+  let p = document.createElement('p')
+  p.textContent = review.username;
+  loopDiv.append(p);
+
+  let p2 = document.createElement('p');
+  p2.textContent = "It was this many starts --> " + review.star;
+  loopDiv.append(p2);
+
+  let p3 = document.createElement('p');
+  p3.textContent = "Review: " + review.review;
+  loopDiv.append(p3);
+
+  
+  reviewsSection.append(review_container);
+  
+
+};
+
+calculateStarAverage(reviews.map(review => review.star));
+reviews.forEach(renderReviews);
+
+
+
+
